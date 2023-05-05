86580b28e43d341b5eac8579eb0ed78c1b58dc8d
dhruvil.d <190540107051@darshan.ac.in>
2023-05-05 15:53:11 +0530

    Learn Resource and Result Filter

---
+++ code/ASP.NET Core Request Processing Pipeline/Filter/ResultFilterLearn/ResultFilterLearn/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ResultFilterLearn.Filters;
+using System.Diagnostics;
+
+namespace ResultFilterLearn.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        [TypeFilter(typeof(CustomResultFilter))]
+        public IActionResult Get()
+        {
+            Debug.WriteLine("Get Method called");
+            return Ok("Get action method called");
+        }
+    }
+}
---
+++ code/ASP.NET Core Request Processing Pipeline/Filter/ResultFilterLearn/ResultFilterLearn/Filters/CustomResultFilter.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Diagnostics;
+
+namespace ResultFilterLearn.Filters
+{
+    public class CustomResultFilter : IAsyncResultFilter
+    {
+        // Action filter is execute before and after action method.
+        // Where Result filter is execute before and after action result not action method.
+        public async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+        {
+            Debug.WriteLine("Result filter execute before result");
+            await next();
+            Debug.WriteLine("Result filter execute after result");
+        }
+    }
+}

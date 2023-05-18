15a679aff8808db7af267cc84d7d156eefdc38f4
dhruvil.d <190540107051@darshan.ac.in>
2023-05-18 15:39:11 +0530

    Solve my route mystery

---
+++ code/ASP.NET Core Request Processing Pipeline/RoutingLearn/RoutingLearn/Controllers/RouteOverride.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoutingLearn.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RouteOverride : ControllerBase
+    {
+        // When we use "/" in starting our existing route override, if we directly start route without "/" then it will append in existing route 
+        [Route("WithoutSlash")] // Route : https://localhost:44336/api/RouteOverride/WhithoutSlash
+        public IActionResult WithoutSlash()
+        {
+            return Ok("WithoutSlash action methods executed");
+        }
+
+        [Route("/WithSlash")] // Route : https://localhost:44336/WhithSlash
+        public IActionResult WithSlash()
+        {
+            return Ok("WithSlash action methods executed");
+        }
+    }
+}
